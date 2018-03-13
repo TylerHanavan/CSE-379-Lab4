b8d5c719a855fb7a66789472dd4d1da15c373305
TylerHanavan <TylerHanavan@gmail.com>
2018-03-13 18:35:31 -0400

    7seg

--- lab4.s
+++ lab4.s
@@ -15,23 +15,23 @@ PIODATA EQU 0x8 ; Offset to parallel I/O data regis
 prompt      = "Welcome to lab #4  ",0      
       ALIGN 
 digits_SET   
-        DCD 0x00001F80  ; 0 
-        DCD 0x00003000  ; 1  
-	DCD 0x00002D80	; 2
-	DCD 0x00002780	; 3
-	DCD 0x00003300	; 4
-	DCD 0x00003680 	; 5
-	DCD 0x00003E80	; 6
-	DCD 0x00000380	; 7
-	DCD 0x00003F80	; 8
-	DCD 0x00003380 	; 9
-	DCD 0x00003B80	; A
-	DCD 0x00003000	; B
-	DCD 0x00001C80	; C
-	DCD 0x00002F00	; D
-	DCD 0x	; E
+        DCD 0x00003780  ; 0 
+        DCD 0x00000300  ; 1  
+	DCD 0x00009580	; 2
+	DCD 0x00008780	; 3
+	DCD 0x0000A300	; 4
+	DCD 0x0000A680 	; 5
+	DCD 0x0000B680	; 6
+	DCD 0x00000738	; 7
+	DCD 0x0000B780	; 8
+	DCD 0x0000A730 	; 9
+	DCD 0x0000B380	; A
+	DCD 0x0000B600	; B
+	DCD 0x0000B400	; C
+	DCD 0x00009700	; D
+	DCD 0x0000B480	; E
                             ; Place other display values here 
-        DCD 0x00003880  ; F 
+        DCD 0x0000B080  ; F 
       ALIGN 
 lab4 
       STMFD SP!,{lr}    ; Store register lr on stack 
